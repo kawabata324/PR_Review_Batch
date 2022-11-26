b86f94fa6bb548414aec462adae6860c6c42985f
kawabata324 <kawabataharuki48@gmail.com>
2022-11-26 15:30:46 +0900

    :sparkles: pr review 拡張機能

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './Options.css';
+import { Box, Flex, Text } from '@mantine/core';
+import { IconQuestionMark } from '@tabler/icons';
 
-interface Props {
-  title: string;
-}
-
-const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+const Options: React.FC = () => {
+  return (
+    <Box
+      component="a"
+      href="https://shrub-music-16e.notion.site/Help-Center-0f58b7fef9af40baa05d305908c3ee1e"
+      target="_blank"
+    >
+      <Flex gap="sm">
+        <IconQuestionMark />
+        <Text>Click here to learn how to use it</Text>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Options;
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -6,8 +6,10 @@ import {
   Center,
   Flex,
   Box,
+  Text,
+  Button,
 } from '@mantine/core';
-import { IconFlame } from '@tabler/icons';
+import { IconFlame, IconQuestionMark, IconConfetti } from '@tabler/icons';
 
 const Popup = () => {
   const initialBatchImages = [
@@ -71,12 +73,28 @@ const Popup = () => {
     },
   ];
 
+  const LGTM_IMAGE = [
+    '![LGTM](https://image.lgtmoon.dev/123319)',
+    '![LGTM](https://image.lgtmoon.dev/117495)',
+    '![LGTM](https://image.lgtmoon.dev/142677)',
+    '![LGTM](https://image.lgtmoon.dev/18220)',
+    '![LGTM](https://image.lgtmoon.dev/36218)',
+    '![LGTM](https://image.lgtmoon.dev/77313)',
+    '![LGTM](https://image.lgtmoon.dev/79249)',
+    '![LGTM](https://image.lgtmoon.dev/177216)',
+  ];
+
   const [batches, setBatches] = useState(initialBatchImages);
+  const [isClickedManual, setClickedManual] = useState(false);
+  const [isClickedRandom, setClickedRandom] = useState(false);
 
   const clickCopyImageUrlToClipboard = (e, text, targetId) => {
     return navigator.clipboard
       .writeText(text)
       .then(() => {
+        setClickedRandom(false);
+        setClickedManual(false);
+
         const clickedBatch = batches.map((batch) => {
           batch.isClicked = batch.id === targetId;
           return { ...batch };
@@ -86,27 +104,75 @@ const Popup = () => {
       .catch((error) => {
         console.log(error);
         setBatches(initialBatchImages);
+        setClickedRandom(false);
+        setClickedManual(false);
+      });
+  };
+
+  const clickCopyManualLgtmImageToClipboard = () => {
+    const lgtmImage = LGTM_IMAGE[Math.floor(Math.random() * LGTM_IMAGE.length)];
+
+    return navigator.clipboard
+      .writeText(lgtmImage)
+      .then(() => {
+        setBatches(initialBatchImages);
+        setClickedRandom(false);
+
+        setClickedManual(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setClickedManual(false);
+      });
+  };
+
+  const clickCopyRandomLgtmImageToClipboard = () => {
+    const max = 40000;
+    const min = 30000;
+    const randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+    return navigator.clipboard
+      .writeText(`![LGTM](https://image.lgtmoon.dev/${randomNum})`)
+      .then(() => {
+        setBatches(initialBatchImages);
+        setClickedManual(false);
+
+        setClickedRandom(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setClickedRandom(false);
       });
   };
 
   return (
     <div className="App">
       <Center>
-        <Flex gap="md" direction="column" align="center">
+        <Flex gap="sm" direction="column" align="center">
           <Blockquote
             cite="– 'reviewer' and 'reviewee' should REVIEW with gratitude!"
             icon={<IconFlame size={24} />}
           >
             We thank each other
           </Blockquote>
-          <SimpleGrid cols={2} pt={10}>
+          <Box
+            component="a"
+            href="https://shrub-music-16e.notion.site/Help-Center-0f58b7fef9af40baa05d305908c3ee1e"
+            target="_blank"
+          >
+            <Flex gap="sm">
+              <IconQuestionMark />
+              <Text>Click here to learn how to use it</Text>
+            </Flex>
+          </Box>
+          <SimpleGrid cols={2}>
             {batches.map((batch) =>
               batch.isClicked ? (
                 <Image
                   radius="sm"
                   width={100}
                   height={20}
-                  src={`https://img.shields.io/badge/copy-${batch.name}-inactive`}
+                  src={`https://img.shields.io/badge/copied-${batch.name}-inactive`}
                   alt="Copy 完了用のイメージ"
                 ></Image>
               ) : (
@@ -128,6 +194,28 @@ const Popup = () => {
               )
             )}
           </SimpleGrid>
+
+          <Flex gap="sm">
+            <Box>
+              <Button
+                leftIcon={<IconConfetti size={14} />}
+                variant={isClickedManual ? 'outline' : 'filled'}
+                onClick={clickCopyManualLgtmImageToClipboard}
+              >
+                {isClickedManual ? 'Copied LGTM' : 'Manual for LGTM'}
+              </Button>
+            </Box>
+
+            <Box>
+              <Button
+                leftIcon={<IconQuestionMark size={14} />}
+                variant={isClickedRandom ? 'outline' : 'filled'}
+                onClick={clickCopyRandomLgtmImageToClipboard}
+              >
+                {isClickedRandom ? 'Copied LGTM' : 'Random for LGTM'}
+              </Button>
+            </Box>
+          </Flex>
         </Flex>
       </Center>
     </div>
