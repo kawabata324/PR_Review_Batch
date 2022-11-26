585cfe0378337119c6f5f72fb7b8056b0b6ee6fe
kawabata324 <kawabataharuki48@gmail.com>
2022-11-26 10:42:42 +0900

    :fire: 必要ないファイル、強制的にリダイレクトさせる機能を削除

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Popup from './Popup';
+import { MantineProvider } from '@mantine/core';
 import './index.css';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+render(
+  <MantineProvider>
+    <Popup />
+  </MantineProvider>,
+  window.document.querySelector('#app-container')
+);
 
 if (module.hot) module.hot.accept();
